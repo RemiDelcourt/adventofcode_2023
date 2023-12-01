ff7e46e2007364c11f7fb4e79db75d18ffd6256a
Rémi Delcourt <aucune@gmail.com>
2023-12-01 14:17:27 +0100

    mep jour 01 suite

---
+++ src/jour_01/Jour01.cpp
@@ -0,0 +1,5 @@
+#include "../jour_01/Jour01.h"
+
+Jour01::Jour01() {
+
+}
---
+++ src/jour_01/Jour01.h
@@ -0,0 +1,11 @@
+#ifndef JOUR01_H
+#define JOUR01_H
+
+#include "../coeur/butano.h"
+
+class Jour01 {
+public:
+    Jour01();
+};
+
+#endif // JOUR01_H
